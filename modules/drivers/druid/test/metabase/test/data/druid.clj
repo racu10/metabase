1035072977826618f2573e2ced9a50fbacaec7bb
Cam Saul <github@camsaul.com>
2020-02-25 16:07:50 -0800

    Simplify druid test extensions namespace [ci druid]

--- modules/drivers/druid/test/metabase/test/data/druid.clj
+++ modules/drivers/druid/test/metabase/test/data/druid.clj
@@ -1,45 +1,14 @@
 (ns metabase.test.data.druid
-  (:require [clojure.string :as str]
-            [metabase.driver.druid.client :as druid.client]
-            [metabase.test.data.interface :as tx]
-            [metabase.util :as u]))
+  (:require [metabase.test.data.interface :as tx]))
 
 (tx/add-test-extensions! :druid)
 
-(defn- host []
-  ;; disable the env var for now so the value in CircleCI (our ec2 server) doesn't stomp on the new localhost one
-  "http://localhost"
-  #_(tx/db-test-env-var-or-throw :druid :host "http://localhost"))
-
-(defn- broker-port []
-  8082
-  #_(Integer/parseUnsignedInt (tx/db-test-env-var-or-throw :druid :port (tx/db-test-env-var-or-throw :druid :broker-port "8082"))))
-
-(defn- broker-port-up? []
-  (u/host-port-up? (str/replace (host) #"^https?://" "") (broker-port)))
-
-(defmethod tx/dbdef->connection-details :druid [])
-
 (defmethod tx/dbdef->connection-details :druid
   [& _]
-  {:host (host)
-   :port (broker-port)})
-
-(defn- datasources-endpoint []
-  (format "%s:%d/druid/v2/datasources" (host) (broker-port)))
-
-(defn- datasources []
-  {:pre [(broker-port-up?)]}
-  (druid.client/GET (datasources-endpoint)))
-
-(defn- already-loaded []
-  (set (datasources)))
+  ;; disable the env var for now so the value in CircleCI (our ec2 server) doesn't stomp on the new localhost one
+  {:host "http://localhost" #_(tx/db-test-env-var-or-throw :druid :host "http://localhost")
+   :port (Integer/parseUnsignedInt (tx/db-test-env-var-or-throw :druid :port "8082"))})
 
 (defmethod tx/create-db! :druid
-  [_ dbdef & _]
-  (let [{:keys [database-name table-definitions], :as dbdef} (tx/get-dataset-definition dbdef)]
-    (assert (= database-name "checkins")
-      "Druid tests currently only support the flattened test-data dataset.")
-    (assert (contains? (already-loaded) "checkins")
-      "Expected 'checkins' dataset to be present in Druid datasources. (This should be loaded as part of building Docker image")
-    nil))
+  [& _]
+  nil)
